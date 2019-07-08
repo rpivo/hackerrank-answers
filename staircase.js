c500317a4c49b3fc03c1687e6781ed3bca41e300
Ryan Pivovar <ryanpivovar@gmail.com>
2019-07-07 17:40:00 -0700

    solve staircase

--- staircase.js
+++ staircase.js
@@ -110,7 +110,14 @@ function readLine() {
 
 // Complete the staircase function below.
 function staircase(n) {
-
+  let returnString = '';
+  let space = ' ';
+  let hashtag = '#';
+  for(i = n; i > 0; i--) {
+    returnString += `${space.repeat(i - 1)}${hashtag.repeat(n - (i - 1))}`;
+    i > 1 && (returnString += `\n`);
+  }
+  return returnString;
 }
 
 /* 
--- test.js
+++ test.js
@@ -1,15 +1,12 @@
-let numbers = [1, 2, -1, -2, 0];
-
-function plusMinus(arr) {
-  let positive = [];
-  let negative = [];
-  let zero     = [];
-  numbers.forEach((number) => {
-    number < 0 ? negative.push(number) :
-    number > 0 ? positive.push(number) :
-    zero.push(number);
-  });
-  return `${positive.length / arr.length}\n${negative.length / arr.length}\n${zero.length / arr.length}`;
+function staircase(n) {
+  let returnString = '';
+  let space = ' ';
+  let hashtag = '#';
+  for(i = n; i > 0; i--) {
+    returnString += `${space.repeat(i - 1)}${hashtag.repeat(n - (i - 1))}`;
+    i > 1 && (returnString += `\n`);
+  }
+  return returnString;
 }
 
-console.log(plusMinus(numbers));
+console.log(staircase(5));
