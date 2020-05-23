34a66aea4380d51f50c33ece64334afc23f1f76e
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-23 06:02:09 -0400

    update staircase

--- staircase.kt
+++ staircase.kt
@@ -1,6 +1,5 @@
 fun staircase(n: Int): Unit {
   for (i in 1..n) {
-    var spaceCount = n - i
-        println(" ".repeat(spaceCount) + "#".repeat(i))
-    }
+    println(" ".repeat(spaceCount) + "#".repeat(n - i))
+  }
 }
